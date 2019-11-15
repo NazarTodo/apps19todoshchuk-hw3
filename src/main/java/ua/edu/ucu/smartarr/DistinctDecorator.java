6821e0265c61d67bbd1cd51eadb9c40de53fe4a5
NazarTodo <todoshchuk@ucu.edu.ua>
2019-11-15 11:33:36 +0200

    Second commit

--- src/main/java/ua/edu/ucu/smartarr/DistinctDecorator.java
+++ src/main/java/ua/edu/ucu/smartarr/DistinctDecorator.java
@@ -31,9 +31,9 @@ public class DistinctDecorator extends SmartArrayDecorator {
                         newArray[j] = null;
                     }
                     if (newArray[i] instanceof Student && newArray[j]
-                            instanceof Student &&
-                            compareStudent((Student) newArray[i],
-                                    (Student) newArray[j])) {
+                            instanceof Student
+                            && compareStudent((Student) newArray[i],
+                            (Student) newArray[j])) {
                         newArray[j] = null;
                     }
 
